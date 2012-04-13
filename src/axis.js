b1893baec1bcab6a6c10658c37fcc353605db299
Mike Bostock <mbostock@gmail.com>
2012-04-13 14:47:10 -0700

    Display the focused time in the axis.

--- src/axis.js
+++ src/axis.js
@@ -1,11 +1,43 @@
 cubism_context.prototype.axis = function() {
   var context = this,
-      axis_ = d3.svg.axis().scale(context.scale);
+      scale = context.scale,
+      axis_ = d3.svg.axis().scale(scale),
+      format = context.step() < 6e4 ? cubism_axisFormatSeconds : cubism_axisFormatMinutes;
 
   function axis(selection) {
-    context.on("change.axis-" + ++cubism_id, function() {
-      selection.call(axis_);
+    var id = ++cubism_id,
+        tick;
+
+    var g = selection.append("svg")
+        .attr("width", context.size())
+        .attr("height", Math.max(28, -axis.tickSize()))
+      .append("g")
+        .attr("transform", "translate(0," + (axis_.orient() === "top" ? 27 : 4) + ")")
+        .call(axis_);
+
+    context.on("change.axis-" + id, function() {
+      g.call(axis_);
+      if (!tick) tick = cloneTick();
     });
+
+    context.on("focus.axis-" + id, function(i) {
+      if (tick) {
+        if (i == null) {
+          tick.style("display", "none");
+          g.selectAll("text").style("fill-opacity", null);
+        } else {
+          tick.style("display", null).attr("x", i).text(format(scale.invert(i)));
+          var dx = tick.node().getComputedTextLength() + 6;
+          g.selectAll("text").style("fill-opacity", function(d) { return Math.abs(scale(d) - i) < dx ? 0 : 1; });
+        }
+      }
+    });
+
+    function cloneTick() {
+      return g.select(function() { return this.appendChild(g.select("text").node().cloneNode()); })
+          .style("display", "none")
+          .text(null);
+    }
   }
 
   return d3.rebind(axis, axis_,
@@ -16,3 +48,6 @@ cubism_context.prototype.axis = function() {
       "tickPadding",
       "tickFormat");
 };
+
+var cubism_axisFormatSeconds = d3.time.format("%I:%M:%S %p"),
+    cubism_axisFormatMinutes = d3.time.format("%I:%M %p");
