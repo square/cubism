4bc6045ea89e9d6fe88c95c53245f70bf83b3506
Roland Huß <roland@jolokia.org>
2012-06-04 10:02:42 +0200

    worked on and added 'delta' and 'keepDelay' options + revised data mapping algorithm

--- src/jolokia.js
+++ src/jolokia.js
@@ -1,23 +1,50 @@
 cubism_contextPrototype.jolokia = function(url,opts) {
-    opts = opts || {};
     var source = {},
-        j4p = new Jolokia({url: url}), // client stub for accessing jolokia
-        step = 5e3,                    // 5 seconds by default
-        id = 0,
-        data = {};
+        context = this,
+        j4p = createAgent(url,opts),
+        step = 5e3;                    // 5 seconds by default
 
-    source.metric = function(request) {
-        id++;
-        var values = data[id];
-        if (!values) {
-            data[id] = values = [];
-        }
+    source.metric = function() {
+        var values = [];
         // If the first argument is a function, this callback function is used for calculating the
-        // value of a datapoint. The remaining arguments should be one or more Jolokia requests objects
+        // value of a data point. The remaining arguments should be one or more Jolokia requests objects
         // and the callback function given will be called with as many response objects after each server query.
         // The callback function needs to return a single calculated numerical value
-        if (typeof request == 'function') {
+        var name;
+        var argsLen = arguments.length;
+        var options = {};
 
+        // Create metric upfront so that it can be used in extraction functions. The name defaults to the mbean name
+        // but can be given as first arguments
+
+        if (typeof arguments[arguments.length - 1] == "string") {
+            name = arguments[arguments.length - 1];
+            argsLen = arguments.length - 1;
+        }
+        // Options can be given as an object
+        if (typeof arguments[arguments.length - 1] == "object" && !arguments[arguments.length - 1].type) {
+            options = arguments[arguments.length - 1];
+            name = options.name;
+            argsLen = arguments.length - 1;
+        }
+        if (!name && typeof arguments[0] != "function") {
+            name = arguments[0].mbean;
+        }
+        var metric;
+        if (options.delta) {
+            // Use cubism metric chaining for calculating the difference value and keep care that the
+            // metric keeps old values up to the delta value
+            var absMetric = context.metric(mapValuesFunc(values,options.keepDelay || options.delta), name);
+            var prevMetric = absMetric.shift(-options.delta);
+            metric = absMetric.subtract(prevMetric);
+        } else {
+            metric =  context.metric(mapValuesFunc(values,options.keepDelay), name);
+        }
+
+        // If an extraction function is given, this can be used for fine grained manipulations of
+        // the answer
+        if (typeof arguments[0] == "function") {
+            var func = arguments[0];
             var respFunc = function(resp) {
                 var isError = false;
                 for (var i = 0; i < arguments.length; i++) {
@@ -27,68 +54,27 @@ cubism_contextPrototype.jolokia = function(url,opts) {
                     }
                 }
                 values.unshift(
-                    isError ?
-                    { time: Date.now(), value: NaN } :
-                    { time: resp.timestamp * 1000, value: request.apply(j4p,arguments) }
+                    { time: Date.now(), value: isError ? NaN : func.apply(metric,arguments) }
                 );
             };
             var args = [ respFunc ];
-            for (var i = 1; i < arguments.length; i++) {
+            for (var i = 1; i < argsLen; i++) {
                 args.push(arguments[i]);
             }
             j4p.register.apply(j4p,args);
         } else {
             // Register the argument given directly as a Jolokia request. The request must return a single
             // numerical value
+            var request = arguments[0];
             j4p.register(function(resp) {
                 values.unshift({
-                    time: resp.timestamp ? resp.timestamp * 1000 : Date.now(),
+                    time: Date.now(),
                     value: j4p.isError(resp) ? NaN : Number(resp.value)
                 });
             },request);
         }
-        return context.metric(function(start, stop, step, callback) {
-            start = +start;
-            stop = +stop;
-            var retVals = [],
-                cTime = stop,
-                len = values.length,
-                cVal = values[0],
-                idx = 0;
 
-            if (!len) {
-                // Nothing fetched yet
-                for (var t = start; t <= stop; t += step) {
-                    retVals.push(NaN);
-                }
-                return callback(null,retVals);
-            }
-            // Fill up wit NaN until we reach the first stored val
-            while (cVal.time < cTime) {
-                retVals.unshift(NaN);
-                cTime -= step;
-            }
-            // Now fill up the data
-            while (cTime >= start && idx < len) {
-                if (cVal.time > cTime && idx < len - 1) {
-                    cVal = values[++idx];
-                }
-                retVals.unshift(cVal.value);
-                cTime -= step;
-            }
-            // Finally prepend with 'NaN' for data not yet fetched
-            while (cTime >= start) {
-                retVals.unshift(NaN);
-                cTime -= step;
-            }
-
-            // Remove older values
-            if (idx < len - 1) {
-                values.length = idx+1;
-            }
-
-            callback(null, retVals);
-        }, request.mbean);
+        return metric;
     };
 
     // Start up fetching of values in the background
@@ -106,4 +92,88 @@ cubism_contextPrototype.jolokia = function(url,opts) {
     // Startup poller which will call the agent periodically
     return source;
 
+    // =======================================================================================
+    // Private helper method
+
+    // Create a new Jolokia agent or reuse a given one
+    function createAgent(url,opts) {
+        if (url instanceof Jolokia) {
+            return url;
+        } else {
+            var args;
+            if (typeof url == "string") {
+                args = {url:url};
+                if (opts) {
+                    for (var key in opts) {
+                        if (opts.hasOwnProperty(key)) {
+                            args[key] = opts[key];
+                        }
+                    }
+                }
+            } else {
+                args = url;
+            }
+            return new Jolokia(args);
+        }
+    }
+
+    // Generate function which picks the requested values from the values
+    // stored periodically by the Jolokia poller.
+    function mapValuesFunc(values,keepDelay) {
+        return function(cStart, cStop, cStep, callback) {
+            cStart = +cStart;
+            cStop = +cStop;
+            var retVals = [],
+                cTime = cStop,
+                vLen = values.length,
+                vIdx = 0,
+                vStart = vLen > 0 ? values[vLen-1].time : undefined;
+
+            if (!vLen || cStop < vStart) {
+                // Nothing fetched yet or seeked interval doesn't overlap with stored values --> return only NaNs
+                for (var t = cStart; t <= cStop; t += cStep) {
+                    retVals.push(NaN);
+                }
+                return callback(null,retVals);
+            }
+
+            // Fill up wit NaN until we reach the first stored val
+            while (cTime > values[0].time + cStep) {
+                retVals.unshift(NaN);
+                cTime -= cStep;
+            }
+
+            while (cTime >= cStart && cTime >= vStart) {
+                // Count down stored values until we find the next best 'fit'
+                // (equals or closest before the step-calculated ime)
+                while (values[vIdx].time > cTime) {
+                    vIdx++;
+                }
+                retVals.unshift(values[vIdx].value);
+                cTime -= cStep;
+            }
+
+            // Finally prepend with 'NaN' for data not yet fetched
+            while (cTime >= cStart) {
+                retVals.unshift(NaN);
+                cTime -= cStep;
+            }
+
+            // Remove older values
+            if (vIdx < vLen - 1) {
+                if (!keepDelay) {
+                    values.length = vIdx+1;
+                } else {
+                    var keepUntil = values[vIdx].time - keepDelay,
+                        i = vIdx;
+                    while (i < vLen && values[i].time > keepUntil) {
+                        i++;
+                    }
+                    values.length = i;
+                }
+            }
+            callback(null, retVals);
+        }
+    }
 };
+
