cce416b16426b6c75fd0c03ec1182329ba523e54
Steve Cai <steve.cai@hulu.com>
2013-04-10 12:27:55 -0700

    a simple linechart implementation.

--- src/line-chart.js
+++ src/line-chart.js
@@ -1,9 +1,8 @@
 cubism_contextPrototype.linechart = function() {
   var context = this,
-      mode = "offset",
-      buffer = document.createElement("canvas"),
-      width = buffer.width = context.size(),
-      height = buffer.height = 30,
+      width = context.size(),
+      height = 30,
+      line = d3.svg.line().interpolate("basis"),
       scale = d3.scale.linear().interpolate(d3.interpolateRound),
       metric = cubism_identity,
       extent = null,
@@ -17,7 +16,7 @@ cubism_contextPrototype.linechart = function() {
         .on("mousemove.linechart", function() { context.focus(Math.round(d3.mouse(this)[0])); })
         .on("mouseout.linechart", function() { context.focus(null); });
 
-    selection.append("canvas")
+    selection.append("svg")
         .attr("width", width)
         .attr("height", height);
 
@@ -29,97 +28,48 @@ cubism_contextPrototype.linechart = function() {
         .attr("class", "value");
 
     selection.each(function(d, i) {
+
       var that = this,
           id = ++cubism_id,
           metric_ = typeof metric === "function" ? metric.call(that, d, i) : metric,
           colors_ = typeof colors === "function" ? colors.call(that, d, i) : colors,
           extent_ = typeof extent === "function" ? extent.call(that, d, i) : extent,
-          start = -Infinity,
           step = context.step(),
-          canvas = d3.select(that).select("canvas"),
+          svg = d3.select(that).select("svg"),
           span = d3.select(that).select(".value"),
           max_,
           m = colors_.length >> 1,
           ready;
 
-      canvas.datum({id: id, metric: metric_});
-      canvas = canvas.node().getContext("2d");
-
-      function change(start1, stop) {
-        canvas.save();
-
+      function change() {
         // compute the new extent and ready flag
-        var extent = metric_.extent();
+        var extent = metric_.extent(),
+            data = [],
+            value = 0;
+
         ready = extent.every(isFinite);
         if (extent_ != null) extent = extent_;
 
-        // if this is an update (with no extent change), copy old values!
-        var i0 = 0, max = Math.max(-extent[0], extent[1]);
-        if (this === context) {
-          if (max == max_) {
-            i0 = width - cubism_metricOverlap;
-            var dx = (start1 - start) / step;
-            if (dx < width) {
-              var canvas0 = buffer.getContext("2d");
-              canvas0.clearRect(0, 0, width, height);
-              canvas0.drawImage(canvas.canvas, dx, 0, width - dx, height, 0, 0, width - dx, height);
-              canvas.clearRect(0, 0, width, height);
-              canvas.drawImage(canvas0.canvas, 0, 0);
-            }
-          }
-          start = start1;
-        }
-
-        // update the domain
-        scale.domain([0, max_ = max]);
-
-        // clear for the new data
-        canvas.clearRect(i0, 0, width - i0, height);
-
-        // record whether there are negative values to display
-        var negative;
-
-        // positive bands
-        for (var j = 0; j < m; ++j) {
-          canvas.fillStyle = colors_[m + j];
-
-          // Adjust the range based on the current band index.
-          var y0 = (j - m + 1) * height;
-          scale.range([m * height + y0, y0]);
-          y0 = scale(0);
-
-          for (var i = i0, n = width, y1; i < n; ++i) {
-            y1 = metric_.valueAt(i);
-            if (y1 <= 0) { negative = true; continue; }
-            canvas.fillRect(i, y1 = scale(y1), 1, y0 - y1);
+        for (var i = 0; i < width; ++i) {
+          value = metric_.valueAt(i);
+          if (value) {
+            data.push(value);
+          } else {
+            data.push(0);
           }
         }
 
-        if (negative) {
-          // enable offset mode
-          if (mode === "offset") {
-            canvas.translate(0, height);
-            canvas.scale(1, -1);
-          }
+        var x = d3.scale.linear().domain([0, data.length]).range([0, width]);
+        var y = scale.domain([d3.max(data), 0]).range([0, height]);
 
-          // negative bands
-          for (var j = 0; j < m; ++j) {
-            canvas.fillStyle = colors_[m - 1 - j];
+        line.x(function(d, i) { return x(i); })
+            .y(function(d) { return y(d); });
 
-            // Adjust the range based on the current band index.
-            var y0 = (j - m + 1) * height;
-            scale.range([m * height + y0, y0]);
-            y0 = scale(0);
-
-            for (var i = i0, n = width, y1; i < n; ++i) {
-              y1 = metric_.valueAt(i);
-              if (y1 >= 0) continue;
-              canvas.fillRect(i, scale(-y1), 1, y0 - scale(-y1));
-            }
-          }
-        }
-
-        canvas.restore();
+        svg.selectAll("path").remove()
+        svg.append("path").attr("d", line(data))
+          .attr("stroke", colors_[m])
+          .attr("stroke-width", 1)
+          .attr("fill", "none");
       }
 
       function focus(i) {
@@ -137,7 +87,7 @@ cubism_contextPrototype.linechart = function() {
       // Note that someone still needs to listen to the metric,
       // so that it continues to update automatically.
       metric_.on("change.linechart-" + id, function(start, stop) {
-        change(start, stop), focus();
+        change(), focus();
         if (ready) metric_.on("change.linechart-" + id, cubism_identity);
       });
     });
@@ -149,7 +99,7 @@ cubism_contextPrototype.linechart = function() {
         .on("mousemove.linechart", null)
         .on("mouseout.linechart", null);
 
-    selection.selectAll("canvas")
+    selection.selectAll("svg")
         .each(remove)
         .remove();
 
@@ -171,7 +121,7 @@ cubism_contextPrototype.linechart = function() {
 
   linechart.height = function(_) {
     if (!arguments.length) return height;
-    buffer.height = height = +_;
+    height = +_;
     return linechart;
   };
 
